dd0c0592e69ffba46653d1426a150821a748a8de
woongcheol Shin <5starshin@gmail.com>
2023-10-19 22:24:48 +0900

    feat: jsx 단순 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Hello from "./chapter1/1-2/Hello";
+import "./App.css";
 
 function App() {
+  const name = "react";
+  const style = {
+    backgroundColor: "black",
+    color: "aqua",
+    fontSize: 24,
+    padding: "1rem",
+  };
+
   return (
-    <div>
+    <>
       <Hello />
-      <Hello />
-      <Hello />
-    </div>
+
+      {/* JSX 안에 자바스크립트 값 적용 */}
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
   );
 }
 
